4f7cdf05c73d73397a64429da221a883dfcb683e
Emil Motrøen <e.motroen@gmail.com>
2024-03-19 15:31:11 +0100

    Add project files.

---
+++ TSD2491-MVC-Test/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TSD2491_MVC_Test.Models;
+
+namespace TSD2491_MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        static MatchingGameModel model = new MatchingGameModel();
+
+        public IActionResult Index()
+        {
+            var model = new MatchingGameModel()
+            {
+                ShuffledAnimals = GetShuffledAnimals(),
+                MatchesFound = GetMatchesFound(),
+                TimeDisplay = GetTimeDisplay()
+            };
+
+            return View(model);
+        }
+
+        public List<string> GetShuffledAnimals()
+        {
+            return model.GetShuffledAnimals();
+        }
+
+        public IActionResult ShuffleAnimals()
+        {
+            return View(model.SetupGame());
+        }
+
+        public int GetMatchesFound()
+        {
+            return model.MatchesFound;
+        }
+
+        public int GetTimeDisplay()
+        {
+            return model.TimeDisplay;
+        }
+
+        [HttpPost]
+        public IActionResult ButtonClick(string animal, string uniqueDescription)
+        {
+            model.ButtonClick(animal, uniqueDescription);
+            var response = new
+            {
+                MatchesFound = GetMatchesFound(),
+                TimeDisplay = GetTimeDisplay()
+            };
+
+            //return Json(response);
+            return RedirectToAction("Index");
+
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TSD2491-MVC-Test/Models/MatchingGameModel.cs
@@ -0,0 +1,83 @@
+﻿namespace TSD2491_MVC_Test.Models
+{
+    public class MatchingGameModel
+    {
+        public int MatchesFound = 0;
+        public int TimeDisplay = 0;
+        public List<string> animalEmoji = new List<string>()
+        {
+            "🐺", "🐺",
+            "🦖", "🦖",
+            "🕷", "🕷",
+            "🐒", "🐒",
+            "🦊", "🦊",
+            "🦌", "🦌",
+            "🦈", "🦈",
+            "🐢", "🐢"
+        };
+
+        public List<string> ShuffledAnimals = new List<string>();
+
+        public List<string> GetShuffledAnimals()
+        {
+            return ShuffledAnimals;
+        }
+
+        public int GetMatchesFound()
+        {
+            return MatchesFound;
+        }
+
+        public int GetTimeDisplay()
+        {
+            return TimeDisplay;
+        }
+
+        public List<string> SetupGame()
+        {
+            MatchesFound = 0;
+            Random random = new Random();
+            return ShuffledAnimals = animalEmoji.OrderBy(item => random.Next()).ToList();
+        }
+
+        public List<string> ButtonClick(string animal, string animalDescription)
+        {
+            string lastAnimalFound = string.Empty;
+            string lastDescription = string.Empty;
+
+            if (lastAnimalFound == string.Empty)
+            {
+                lastAnimalFound = animal;
+                lastDescription = animalDescription;
+
+            }
+            else if ((lastAnimalFound == animal) && (animalDescription != lastDescription))
+            {
+                // Match found! Reset for next pair.
+                lastAnimalFound = string.Empty;
+
+                // Replace found animals with empty string to hide them.
+                ShuffledAnimals = ShuffledAnimals
+                    .Select(a => a.Replace(animal, string.Empty))
+                    .ToList();
+
+                MatchesFound++;
+                if (MatchesFound == 8)
+                {
+                    //timer.Stop();
+                    //timeDisplay += " - Play Again?";
+
+                    SetupGame();
+                }
+            }
+            else
+            {
+                // User selected a pair that don't match.
+                // Reset selection.
+                lastAnimalFound = string.Empty;
+            }
+
+            return ShuffledAnimals;
+        }
+    }
+}
