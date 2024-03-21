e2d50009754aa3f16e4f438561d6493bc39fe763
Emil Motrøen <e.motroen@gmail.com>
2024-03-21 19:41:00 +0100

    Added inital code for the memory game

--- TSD2491-MVC-Test/Models/MemoryGameModel.cs
+++ TSD2491-MVC-Test/Models/MemoryGameModel.cs
@@ -1,7 +1,55 @@
-﻿namespace TSD2491_MVC_Test.Models
+﻿using System;
+
+namespace TSD2491_MVC_Test.Models
 {
     public class MemoryGameModel
     {
+        private static Random random = new Random();
 
+        private int matchesFound = 0;
+
+        private bool gameFinished = false;
+
+        private const string gameRunningText = "Memorize the emojies!";
+        private const string gameWonText = "Game Complete!";
+
+        private readonly static List<string> travelEmoji = new List<string>()
+        {
+            "🚲", "🚲",
+            "🎡", "🎡",
+            "⛩", "⛩",
+            "🌠", "🌠",
+            "🎠", "🎠",
+            "⛪️", "⛪️",
+            "🌌", "🌌",
+            "🗾", "🗾"
+        };
+
+        public List<string> ShuffledEmoji = travelEmoji.OrderBy(item => random.Next()).ToList();
+
+        public bool GetGameFinished()
+        {
+            return gameFinished;
+        }
+
+        public int GetMatchesFound()
+        {
+            return matchesFound;
+        }
+
+        public string GetGameRunningText()
+        {
+            return gameRunningText;
+        }
+
+        public string GetGameFinishedText()
+        {
+            return gameWonText;
+        }
+
+        public void ButtonClick(string emoji, string uniqueDescription)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
